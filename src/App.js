3cfdc23a3a4c155c923679dd5b99dde765df9738
ipekmuhammet <muhammetipek57@hotmail.com>
2020-06-12 19:27:35 +0300

    bootstrap login

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { Switch, BrowserRouter, Route } from 'react-router-dom'
 
 import Screen1 from './screens/Screen1'
 import Screen2 from './screens/Screen2'
-import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import Login from './screens/Login'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path={'/'} component={Screen1} />
-				<Route path={'/login'} component={LoginScreen} />
+				<Route path={'/login'} component={Login} />
 				<Route path={'/register'} component={RegisterScreen} />
 				<Route path={'/details/:id'} component={Screen2} />
+
 			</Switch>
 		</BrowserRouter>
 	)
---
+++ src/screens/Login.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import axios from 'axios'
+import Cookies from 'universal-cookie'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const cookies = new Cookies()
+
+class Login extends React.Component {
+
+    state = {
+        phoneNumber: '905468133191',
+        password: '1234'
+    }
+
+    onPhoneChange = (event) => {
+        this.setState({ phoneNumber: event.target.value })
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({ password: event.target.value })
+    }
+
+    onLoginClick = () => {
+        axios.post('http://127.0.0.1:3000/login-manager', {
+            phoneNumber: this.state.phoneNumber,
+            password: this.state.password
+        }).then((response) => {
+            if (response.data.token) {
+                cookies.set('token', response.data.token)
+                this.props.history.push('/')
+            } else {
+                alert('Şifre veya telefon numarası yanlış!')
+            }
+        }).catch((err) => {
+            console.log('err', err)
+            alert('Beklenmedik bir hata oluştu!')
+        })
+    }
+
+    onRegisterClick = () => {
+        this.props.history.push('/register')
+    }
+
+    render() {
+        return (
+            <div style={{ height: '100vh' }}>
+                <div className='col-md-12 h-100 d-flex align-items-center justify-content-center'>
+                    <div className='m-4 p-4 w-25 border'>
+                        <div className='form-group row'>
+                            <div className='col-md-12'>
+                                <input
+                                    type='text'
+                                    placeholder='Telefon Numarası'
+                                    onChange={this.onPhoneChange}
+                                    value={this.state.phoneNumber}
+                                    className='form-control'
+                                />
+                            </div>
+                        </div>
+                        <div className='form-group row'>
+                            <div className='col-md-12'>
+                                <input
+                                    type='text'
+                                    placeholder='Şifre'
+                                    onChange={this.onPasswordChange}
+                                    value={this.state.password}
+                                    className='form-control' />
+                            </div>
+                        </div>
+                        <div className='form-group row'>
+                            <div className='col-md-6'>
+                                <button
+                                    type='text'
+                                    onClick={this.onLoginClick}
+                                    className='btn btn-primary btn-block'>
+                                    Giriş Yap
+                                </button>
+                            </div>
+                            <div className='col-md-6'>
+                                <button
+                                    type='text'
+                                    onClick={this.onRegisterClick}
+                                    className='btn btn-primary btn-block'>
+                                    Kayıt Ol
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
